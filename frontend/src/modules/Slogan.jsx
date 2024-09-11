3bcb0657c73d8c851797ecb49533a1ec1fb6a6be
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2024-09-11 13:01:43 -0700

    Delete frontend directory

--- frontend/src/modules/Slogan.jsx
+++
@@ -1,9 +0,0 @@
-function Slogan() {
-    return (
-       <>
-           <p>Quiz to see how my cognitive learning styles compare to 
-            typical personas.</p>
-       </>
-    );
-}
-export default Slogan;
