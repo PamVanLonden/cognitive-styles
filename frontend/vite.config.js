37ba08cf3ecdbdaf3fb1c5090ba7fe0f660ef9cc
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2024-09-12 14:28:14 -0700

    new package and config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    target: 'esnext',
-    platform: 'linux',
-  }
+  plugins: [react()] 
 });
