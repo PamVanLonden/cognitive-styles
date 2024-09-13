9f51820f155fc24dcf81cc46fd57e52fb3ae1ab0
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2024-09-13 11:30:48 -0700

    utility updates

--- frontend/src/modules/utils/SurveyContext.jsx
+++ frontend/src/modules/utils/SurveyContext.jsx
@@ -7,17 +7,28 @@ export const SurveyProvider = ({ children }) => {
   const [surveyData, setSurveyData] = useState({});
 
   // Function to add/update survey data
-  const addSurveyData = (field, value) => {
-    setSurveyData(prev => ({ ...prev, [field]: value }));
-  };
-
-
-  // const addSurveyData = (newData) => {
-  //   setSurveyData((prevData) => ({
-  //     ...prevData,
-  //     ...newData,
-  //   }));
+  // const addSurveyData = (field, value) => {
+  //   setSurveyData(prev => ({ ...prev, [field]: value }));
   // };
+  
+ // Function to add/update survey data
+const addSurveyData = (fieldOrData, value) => {
+  if (typeof fieldOrData === 'object') {
+    // Batch update if an object is passed
+    setSurveyData(prev => ({ ...prev, ...fieldOrData }));
+  } else {
+    // Single field update
+    setSurveyData(prev => ({ ...prev, [fieldOrData]: value }));
+  }
+};
+
+
+// const addSurveyData = (newData) => {
+//    setSurveyData((prevData) => ({
+//      ...prevData,
+//      ...newData,
+//     }));
+//   };
 
   return (
     <SurveyContext.Provider value={{ surveyData, addSurveyData }}>
