6ccab8a31bebb840ea450d1b24a14ed7a0c79d7f
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2024-10-01 13:42:41 -0700

    Added file compression to screenshot file.

--- frontend/src/modules/utils/ScreenshotButton.jsx
+++ frontend/src/modules/utils/ScreenshotButton.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import html2canvas from 'html2canvas';
+import Pica from 'pica';
 import { PiDownloadFill } from "react-icons/pi";
 
 export const ScreenshotButton = () => {
-  const takeScreenshot = () => {
+  const takeScreenshot = async () => {
     const mainElement = document.querySelector('main');
     if (mainElement) {
-      html2canvas(mainElement).then((canvas) => {
-        // Convert the canvas to a Blob (for save-as dialog)
-        canvas.toBlob((blob) => {
-          const link = document.createElement('a');
-          link.href = URL.createObjectURL(blob);
-          link.download = 'cognitive-facets-survey.png'; // Filename for the downloaded image
-          link.click(); // Trigger the download
-        });
+      const canvas = await html2canvas(mainElement);
+      const pica = new Pica();
+      const outputCanvas = document.createElement('canvas');
+      
+      // Set dimensions for the output canvas (smaller dimensions for compression)
+      outputCanvas.width = canvas.width / 2;  // Adjust the factor as needed
+      outputCanvas.height = canvas.height / 2; // Adjust the factor as needed
+
+      // Resize the image using Pica
+      pica.resize(canvas, outputCanvas).then(() => {
+        return pica.toBlob(outputCanvas, 'image/png', 0.8); // Adjust quality
+      }).then((blob) => {
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'cognitive-facets-survey-summary.png';
+        link.click();
       });
     }
   };
