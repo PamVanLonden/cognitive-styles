ba4047ab11bb4903c06b7d64db356e01567b84d9
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2024-10-03 12:34:40 -0700

    updated syntax

--- frontend/src/modules/data/promptsObject.js
+++ frontend/src/modules/data/promptsObject.js
@@ -1,21 +1,14 @@
 const promptsObject = [
-    { text: "Describe your Cognitive Style score ranges." },
-    { text: "How are your facet values similar or different from another person who shared theirs?" },
-    { text: "What is one big thing you and your teammates have in common?" },
-    { text: "What is one big thing you and your teammates do differently?" },
-    { text: "How did your cognitive styles affect how you approached the previous assignment?" },
-    { text: "Name a specific task or two that you hesitate to tackle. Why, and what kind of support would help you overcome the hesitation?" },
-    { text: "Give a concrete example for one or more facets of how the style affects your interactions with technology or how you learn." },
-    { text: "Give a concrete example for one or more facets of how you work in a team." },
-    { text: "How might your cognitive styles affect how you learn a new skill?" },
-    { text: "What is a situation when your facet values might change?" }
+     "Describe your Cognitive Style score ranges.",
+     "How are your facet values similar or different from another person who shared theirs?",
+     "What is one big thing you and your teammates have in common?",
+     "What is one big thing you and your teammates do differently?",
+     "How did your cognitive styles affect how you approached the previous assignment?",
+     "Name a specific task or two that you hesitate to tackle. Why, and what kind of support would help you overcome the hesitation?",
+     "Give a concrete example for one or more facets of how the style affects your interactions with technology or how you learn.",
+     "Give a concrete example for one or more facets of how you work in a team.",
+     "How might your cognitive styles affect how you learn a new skill?",
+     "What is a situation when your facet values might change?" 
 ];
 
-// Automatically add IDs
-const promptsWithIds = promptsObject.map((prompt, index) => ({
-    ...prompt,
-    id: index + 1
-}));
-
-// Export this as the default
-export default promptsWithIds;
+export default promptsObject;
