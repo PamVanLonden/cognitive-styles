8caaaa7151bcac6813a4a5d53fd17f24f15ebf41
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2024-10-18 14:18:35 -0700

    removed sitemap code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  sitemap: {
-    hostname: 'https://cognitive-styles.onrender.com/',
-    lastUpdated: true
-  }
+  plugins: [react()]
 });
