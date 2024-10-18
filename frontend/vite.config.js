53be598e3ac8a1a4235f6d8bfbf6ccd9837974af
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2024-10-18 14:07:00 -0700

    sitemap updaes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  sitemap: {
+    hostname: 'https://cognitive-styles.onrender.com/',
+    lastUpdated: true
+  }
 });
